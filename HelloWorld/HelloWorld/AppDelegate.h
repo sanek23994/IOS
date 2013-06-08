7b34537c2a85fe3d5708da84adccc6c1c03cd735
Alexander Ovchinnikov <sanek23994@gmail.com>
2013-06-08 14:00:23 +0400

    Try to create HelloWorld

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Alexander Ovchinnikov on 6/8/13.
+//  Copyright (c) 2013 Alexander Ovchinnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Alexander Ovchinnikov on 6/8/13.
+//  Copyright (c) 2013 Alexander Ovchinnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
